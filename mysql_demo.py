f0dc79a356b8832e5bc2c27335027d91092bad8c
Khurramjaved <kjaved@ualberta.ca>
2021-10-17 21:12:21 -0600

    Push sample of a parameter sweep where data is logged in a databaseA

---
+++ mysql_demo.py
@@ -0,0 +1,20 @@
+import random
+import configs.experiment_parameters as reg_parser
+import utils
+from experiment import ExperimentManager, Metric
+
+p = reg_parser.ExperimentOne()
+all_args = vars(p.parse_known_args()[0])
+args = utils.get_run(all_args, p.parse_known_args()[0].run)
+
+my_experiment = ExperimentManager(args["name"], args, "khurram")
+
+error_logger = Metric("error", {"run": 0, "error": 0.0, "accuracy": 0.0, "step": 0}, ("run", "step"), my_experiment)
+predictions_logger = Metric("predictions", {"run": 0, "prediction": 0.0, "step": 0}, ("run", "step"), my_experiment)
+
+for step in range(0, 100):
+    error_logger.add_data([my_experiment.run, random.random(), random.random() * 100, step])
+    predictions_logger.add_data([my_experiment.run, random.random() * 10, step])
+
+error_logger.commit_to_database()
+predictions_logger.commit_to_database()
