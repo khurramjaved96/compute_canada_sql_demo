a021dfe9bac35f0c110a83fa1b272871d682b25a
Khurramjaved <kjaved@ualberta.ca>
2021-10-17 21:20:34 -0600

    Make CC username field more prominent

--- mysql_demo.py
+++ mysql_demo.py
@@ -3,11 +3,13 @@ import configs.experiment_parameters as reg_parser
 import utils
 from experiment import ExperimentManager, Metric
 
+COMPUTE_CANADA_USERNAME = "khurram"
+
 p = reg_parser.ExperimentOne()
 all_args = vars(p.parse_known_args()[0])
 args = utils.get_run(all_args, p.parse_known_args()[0].run)
 
-my_experiment = ExperimentManager(args["name"], args, "khurram")
+my_experiment = ExperimentManager(args["name"], args, COMPUTE_CANADA_USERNAME)
 
 error_logger = Metric("error", {"run": 0, "error": 0.0, "accuracy": 0.0, "step": 0}, ("run", "step"), my_experiment)
 predictions_logger = Metric("predictions", {"run": 0, "prediction": 0.0, "step": 0}, ("run", "step"), my_experiment)
